0350363ba8bdacb2b94b3fb4f0f6f23c3c0e833c
liangce <lcsmm@163.com>
2020-03-27 11:29:05 +0800

    20200327

--- python_mysql/dbmysql.py
+++ python_mysql/dbmysql.py
@@ -3,9 +3,25 @@
 #author:liangce
 import os,re,sys,datetime
 import MySQLdb
+from getpass import getpass
 
 class dbmg(object):
-      def dbsinfo(self,bhost,bport,buser,bpassword,bdatabase1,bdatabase2,bdatabase3):
+      def __init__(self,bhost='',bport='',buser='',bpassword='',bdatabase1='',bdatabase2='',bdatabase3=''):
+          if not bhost:
+              bhost = (input("请输入源库地址：")).strip()
+          if not bport:
+              bport = int(input("请输入源库端口："))     
+          if not buser:
+              buser = (input("请输入迁移账号：")).strip()
+          if not bpassword:
+              bpassword = getpass("请输入迁移账号密码: ") 
+          if not bdatabase1:
+              bdatabase1 = (input("请输入迁移库名1: ")).strip()
+          if not bdatabase2:
+              bdatabase2 = (input("请输入迁移库名2: ")).strip()
+          if not bdatabase3:
+              bdatabase3 = (input("请输入迁移库名3: ")).strip()
+
           self.bhost = bhost
           self.bport = bport
           self.buser = buser
@@ -21,9 +37,13 @@ class dbmg(object):
           self.rpassword = rpassword
 
       def checkmysql(self):
+          #print(self.bhost)
+          #print(self.bport)
+          #print(self.buser)
+          #print(self.bpassword)
           conn = MySQLdb.connect(host=self.bhost,port=self.bport,user=self.buser,passwd=self.bpassword,db=self.bdatabase1)
           cur  = conn.cursor()
-          sql = "select * from product"
+          sql = "select * from usertb"
           results = cur.execute(sql)
           #aa = cur.fetchmany(results)
           cur.close()
@@ -32,7 +52,5 @@ class dbmg(object):
           print(results)
           #for data in aa:
           #     print(data)
-
 test = dbmg()
-test.dbsinfo('192.168.0.82',3306,'ccc','jys#123','mmm','test','video')
 test.checkmysql()
